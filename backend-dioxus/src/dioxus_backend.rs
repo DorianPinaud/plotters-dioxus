a97b72207ca9c31b486d630d4d752d34ae0a02e5
dorian.pinaud <dorian.pinaud@stilla.fr>
2024-01-28 19:59:50 +0100

    Add rect and line to the plot drawing

--- backend-dioxus/src/dioxus_backend.rs
+++ backend-dioxus/src/dioxus_backend.rs
@@ -8,19 +8,34 @@ use plotters_backend::{
 };
 use std::io::Error;
 
-pub struct DioxusBackend;
+use dioxus::prelude::*;
 
-impl DioxusBackend {
-    pub fn new() -> Self {
-        Self{}
+pub type Stack<'a, 'b> = &'a mut Vec<Box<dyn Fn() -> LazyNodes<'b, 'b>>>;
+
+pub struct DioxusBackend<'a, 'b: 'a> {
+    pub stack: Stack<'a, 'b>,
+}
+
+impl<'a, 'b> DioxusBackend<'a, 'b> {
+    pub fn new(stack: Stack<'a, 'b>) -> Self {
+        Self { stack: stack }
     }
 }
 
-impl<'a> DrawingBackend for DioxusBackend {
+fn make_svg_color(color: BackendColor) -> String {
+    let (r, g, b) = color.rgb;
+    return format!("#{:02X}{:02X}{:02X}", r, g, b);
+}
+
+fn make_svg_opacity(color: BackendColor) -> String {
+    return format!("{}", color.alpha);
+}
+
+impl<'a, 'b> DrawingBackend for DioxusBackend<'a, 'b> {
     type ErrorType = Error;
 
     fn get_size(&self) -> (u32, u32) {
-        (0, 0)
+        (200, 200)
     }
 
     fn ensure_prepared(&mut self) -> Result<(), DrawingErrorKind<Error>> {
@@ -33,28 +48,99 @@ impl<'a> DrawingBackend for DioxusBackend {
 
     fn draw_pixel(
         &mut self,
-        _: BackendCoord,
-        _: BackendColor
+        point: BackendCoord,
+        color: BackendColor
     ) -> Result<(), DrawingErrorKind<Error>> {
+        if color.alpha == 0.0 {
+            return Ok(());
+        }
+        self.stack.push(
+            Box::new(move || {
+                let hex_color = make_svg_color(color);
+                rsx! {
+                    rect {
+                        x: "{point.0}",
+                        y: "{point.1}",
+                        width: 1,
+                        height: 1,
+                        fill: "{hex_color}",
+                        stroke: "none",
+                    }
+                }
+            })
+        );
         Ok(())
     }
 
     fn draw_line<S: BackendStyle>(
         &mut self,
-        _: BackendCoord,
-        _: BackendCoord,
-        _: &S
+        from: BackendCoord,
+        to: BackendCoord,
+        style: &S
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        if style.color().alpha == 0.0 {
+            return Ok(());
+        }
+        let hex_color = make_svg_color(style.color());
+        let alpha = make_svg_opacity(style.color());
+        let stroke_width = format!("{}", style.stroke_width());
+        self.stack.push(
+            Box::new(move || {
+                let hex_color = hex_color.clone();
+                let alpha = alpha.clone();
+                let stroke_width = stroke_width.clone();
+                rsx! {
+                    line {
+                        x1: "{from.0}",
+                        y1: "{from.1}",
+                        x2: "{to.0}",
+                        y2: "{to.1}",
+                        stroke: "{hex_color}",
+                        opacity: "{alpha}",
+                        stroke_width: "{stroke_width}",
+                    }
+                }
+            })
+        );
         Ok(())
     }
 
     fn draw_rect<S: BackendStyle>(
         &mut self,
-        _: BackendCoord,
-        _: BackendCoord,
-        _: &S,
-        _: bool
+        upper_left: BackendCoord,
+        bottom_right: BackendCoord,
+        style: &S,
+        fill: bool
     ) -> Result<(), DrawingErrorKind<Self::ErrorType>> {
+        if style.color().alpha == 0.0 {
+            return Ok(());
+        }
+
+        let (fill, stroke) = if !fill {
+            ("none".to_string(), make_svg_color(style.color()))
+        } else {
+            (make_svg_color(style.color()), "none".to_string())
+        };
+
+        let alpha = make_svg_opacity(style.color());
+        self.stack.push(
+            Box::new(move || {
+                let alpha = alpha.clone();
+                let fill = fill.clone();
+                let stroke = stroke.clone();
+                rsx! {
+                    rect {
+                        x: "{upper_left.0}",
+                        y: "{upper_left.1}",
+                        width: "{bottom_right.0 - upper_left.0}",
+                        height: "{bottom_right.1 - upper_left.1}",
+                        opacity: "{alpha}",
+                        fill: "{fill}",
+                        stroke: "{stroke}",
+                    }
+                }
+            })
+        );
         Ok(())
     }
 
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -7,44 +7,52 @@ fn main() {
     dioxus_desktop::launch(App);
 }
 
-fn App(cx: Scope) -> Element {
-    let root_area = DioxusBackend::new().into_drawing_area();
-    let _ = root_area.fill(&RED);
-    let mut chart = ChartBuilder::on(&root_area)
-        .x_label_area_size(35)
-        .y_label_area_size(40)
-        .margin(5)
-        .caption("Histogram Test", ("sans-serif", 50.0))
-        .build_cartesian_2d((0u32..10u32).into_segmented(), 0u32..10u32)
-        .expect("Expect a chart to be build");
+fn App<'a>(cx: Scope<'a>) -> Element {
+    let mut stack = Vec::<Box<dyn Fn() -> LazyNodes<'a, 'a>>>::new();
 
-    let _ = chart
-        .configure_mesh()
-        .disable_x_mesh()
-        .bold_line_style(WHITE.mix(0.3))
-        .y_desc("Count")
-        .x_desc("Bucket")
-        .axis_desc_style(("sans-serif", 15))
-        .draw();
+    {
+        let root_area = DioxusBackend::new(&mut stack).into_drawing_area();
+        let _ = root_area.fill(&RED);
+        let mut chart = ChartBuilder::on(&root_area)
+            .x_label_area_size(35)
+            .y_label_area_size(40)
+            .margin(5)
+            .caption("Histogram Test", ("sans-serif", 50.0))
+            .build_cartesian_2d((0u32..10u32).into_segmented(), 0u32..10u32)
+            .expect("Expect a chart to be build");
 
-    let data = [0u32, 1, 1, 1, 4, 2, 5, 7, 8, 6, 4, 2, 1, 8, 3, 3, 3, 4, 4, 3, 3, 3];
+        let _ = chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .bold_line_style(WHITE.mix(0.3))
+            .y_desc("Count")
+            .x_desc("Bucket")
+            .axis_desc_style(("sans-serif", 15))
+            .draw();
 
-    let _ = chart.draw_series(
-        Histogram::vertical(&chart)
-            .style(RED.mix(0.5).filled())
-            .data(data.iter().map(|x: &u32| (*x, 1)))
-    );
+        let data = [0u32, 1, 1, 1, 4, 2, 5, 7, 8, 6, 4, 2, 1, 8, 3, 3, 3, 4, 4, 3, 3, 3];
 
-    // To avoid the IO failure being ignored silently, we manually call the present function
-    root_area
-        .present()
-        .expect(
-            "Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir"
+        let _ = chart.draw_series(
+            Histogram::vertical(&chart)
+                .style(RED.mix(0.5).filled())
+                .data(data.iter().map(|x: &u32| (*x, 1)))
         );
 
-    cx.render(rsx! {
-        div {
-            "Hello, world!"
+        // To avoid the IO failure being ignored silently, we manually call the present function
+        root_area
+            .present()
+            .expect(
+                "Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir"
+            );
+    }
+
+    cx.render(
+        rsx! {
+            svg {
+                height: "200",
+                width: "200",
+                stack.iter().map(|e| (*e)()),
+            }
         }
-    })
+    )
 }

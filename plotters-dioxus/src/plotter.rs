9a5ae19bd86cc67f46321f74bc11a7f01bb4e9d5
dorian.pinaud <dorian.pinaud@stilla.fr>
2024-02-26 11:36:33 +0100

    handle the drag events

--- plotters-dioxus/src/plotter.rs
+++ plotters-dioxus/src/plotter.rs
@@ -28,6 +28,16 @@ pub struct PlottersProps<'a, F: Fn(DioxusDrawingArea)> {
     pub on_mousedown: Option<EventHandler<'a, Event<MouseData>>>,
     pub on_mouseover: Option<EventHandler<'a, Event<MouseData>>>,
     pub on_wheel: Option<EventHandler<'a, Event<WheelData>>>,
+    #[props(default = false)]
+    pub draggable: bool,
+    pub on_drag: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_dragend: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_dragenter: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_dragleave: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_dragover: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_dragstart: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_drop: Option<EventHandler<'a, Event<DragData>>>,
+    pub on_scroll: Option<EventHandler<'a, Event<ScrollData>>>,
 }
 
 impl<'a, F: Fn(DioxusDrawingArea)> PartialEq for PlottersProps<'a, F> {
@@ -57,46 +67,92 @@ pub fn Plotters<'a, F: Fn(DioxusDrawingArea)>(cx: Scope<'a, PlottersProps<'a, F>
     let buffer_base64 = BASE64_STANDARD.encode(data);
 
     render!(img {
-        onclick: |e| (
-            if cx.props.on_click.is_some() {
-                cx.props.on_click.as_ref().unwrap().call(e)
-            }
-        ),
-        ondblclick: |e| (
-            if cx.props.on_dblclick.is_some() {
-                cx.props.on_dblclick.as_ref().unwrap().call(e)
-            }
-        ),
-        onmousemove: |e| {
-            if cx.props.on_mousemove.is_some() {
-                cx.props.on_mousemove.as_ref().unwrap().call(e)
-            }
+        onclick: |e| {
+            if let Some(callback) = cx.props.on_click.as_ref() {
+                callback.call(e)
+            };
         },
-        onmousedown: |e| (
-            if cx.props.on_mousedown.is_some() {
-                cx.props.on_mousedown.as_ref().unwrap().call(e)
-            }
-        ),
-        onmouseup: |e| (
-            if cx.props.on_mouseup.is_some() {
-                cx.props.on_mouseup.as_ref().unwrap().call(e)
-            }
-        ),
+        ondblclick: |e| {
+            if let Some(callback) = cx.props.on_dblclick.as_ref() {
+                callback.call(e)
+            };
+        },
+        onmousemove: |e| {
+            if let Some(callback) = cx.props.on_mousemove.as_ref() {
+                callback.call(e)
+            };
+        },
+        onmousedown: |e| {
+            if let Some(callback) = cx.props.on_mousedown.as_ref() {
+                callback.call(e)
+            };
+        },
+        onmouseup: |e| {
+            if let Some(callback) = cx.props.on_mouseup.as_ref() {
+                callback.call(e)
+            };
+        },
         onmouseout: |e| {
-            if cx.props.on_mouseout.is_some() {
-                cx.props.on_mouseout.as_ref().unwrap().call(e)
-            }
+            if let Some(callback) = cx.props.on_mouseout.as_ref() {
+                callback.call(e)
+            };
         },
         onmouseover: |e| {
-            if cx.props.on_mouseover.is_some() {
-                cx.props.on_mouseover.as_ref().unwrap().call(e)
-            }
+            if let Some(callback) = cx.props.on_mouseout.as_ref() {
+                callback.call(e)
+            };
         },
-        onwheel: |e| (
-            if cx.props.on_wheel.is_some() {
-                cx.props.on_wheel.as_ref().unwrap().call(e)
-            }
-        ),
+        onwheel: |e| {
+            if let Some(callback) = cx.props.on_wheel.as_ref() {
+                callback.call(e)
+            };
+        },
+        onwheel: |e| {
+            if let Some(callback) = cx.props.on_wheel.as_ref() {
+                callback.call(e)
+            };
+        },
+        draggable: cx.props.draggable,
+        ondrag: |e| {
+            if let Some(callback) = cx.props.on_drag.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondragend: |e| {
+            if let Some(callback) = cx.props.on_dragend.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondragenter: |e| {
+            if let Some(callback) = cx.props.on_dragenter.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondragleave: |e| {
+            if let Some(callback) = cx.props.on_dragleave.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondragover: |e| {
+            if let Some(callback) = cx.props.on_dragover.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondragstart: |e| {
+            if let Some(callback) = cx.props.on_dragstart.as_ref() {
+                callback.call(e)
+            };
+        },
+        ondrop: |e| {
+            if let Some(callback) = cx.props.on_drop.as_ref() {
+                callback.call(e)
+            };
+        },
+        onscroll: |e| {
+            if let Some(callback) = cx.props.on_scroll.as_ref() {
+                callback.call(e)
+            };
+        },
         src: "data:image/png;base64,{buffer_base64}",
     })
 }
